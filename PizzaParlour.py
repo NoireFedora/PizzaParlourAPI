65b9166714bdeda2122ea159a306e2f6700535ca
Rui Zhu <zhuruisy@gmail.com>
2020-11-04 15:34:29 -0500

    some demo of the api

--- PizzaParlour.py
+++ PizzaParlour.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask("Assignment 2")
 
 @app.route('/pizza')
 def welcome_pizza():
     return 'Welcome to Pizza Planet!'
+
+# first we need a library
+orders = {}
+
+@app.route('/pizza/submut_order', methods = ['POST'])
+def submit_order():
+    content = request.get_json()
+    print(content['Pizza']['Size'])
+    return content
+
+@app.route('/pizza/update_order/<orderid>', methods = ['POST'])
+def update_order(orderid):
+    print(orderid)
+    return "update done"
 
 if __name__ == "__main__":
     app.run()
