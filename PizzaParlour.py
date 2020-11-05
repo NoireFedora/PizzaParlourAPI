30adbd9cc82e48847dc68ce4385bc165e42c9f12
NoireFedora <50502397+NoireFedora@users.noreply.github.com>
2020-11-04 20:11:28 -0500

    Small Demo

--- PizzaParlour.py
+++ PizzaParlour.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask, request
 
 app = Flask("Assignment 2")
@@ -6,19 +7,48 @@ app = Flask("Assignment 2")
 def welcome_pizza():
     return 'Welcome to Pizza Planet!'
 
-# first we need a library
+order_id = 0
 orders = {}
 
-@app.route('/pizza/submut_order', methods = ['POST'])
-def submit_order():
-    content = request.get_json()
-    print(content['Pizza']['Size'])
-    return content
+def print_receipt(orders):
+    for order in orders:
+        print(orders[order])
+        print("\n")
 
-@app.route('/pizza/update_order/<orderid>', methods = ['POST'])
-def update_order(orderid):
-    print(orderid)
-    return "update done"
+@app.route('/pizza/submit_order', methods = ['POST'])
+def submit_order():
+    global order_id
+    pizza_id = 1
+    order_id += 1
+    content = request.get_json()
+    orders[order_id] = content
+    for pizza in orders[order_id]["Pizza"]:
+        pizza["ID"] = pizza_id
+        pizza_id += 1
+    print_receipt(orders)
+    return orders
+
+@app.route('/pizza/update_order/<order_id>', methods = ['POST'])
+def update_order(order_id):
+    if order_id not in orders:
+        status_code = flask.Response(status=404)
+        return status_code
+    content = request.get_json()
+    for order in orders:
+        for modified in content:
+            if modified == order:
+                orders[order] = content
+    print(orders)
+    return "Update Done"
+
+@app.route('/pizza/cancle_order/<order_id>', methods = ['POST'])
+def cancel_order(order_id):
+    if order_id not in orders:
+        status_code = flask.Response(status=404)
+        return status_code
+    del orders[order_id]
+    print(orders)
+    return "Cancel Done"
 
 if __name__ == "__main__":
     app.run()
