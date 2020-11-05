a7f0189171abb83d4f6a65b582f671bc2ba5ee10
NoireFedora <50502397+NoireFedora@users.noreply.github.com>
2020-11-04 23:13:33 -0500

    update method

--- PizzaParlour.py
+++ PizzaParlour.py
@@ -18,15 +18,19 @@ def print_receipt(orders):
 @app.route('/pizza/submit_order', methods = ['POST'])
 def submit_order():
     global order_id
-    pizza_id = 1
     order_id += 1
     content = request.get_json()
     orders[order_id] = content
-    for pizza in orders[order_id]["Pizza"]:
-        pizza["ID"] = pizza_id
-        pizza_id += 1
     print_receipt(orders)
     return orders
+
+@app.route('/pizza/get_order/<order_id>', methods = ['GET'])
+def get_order(order_id):
+    if order_id not in orders:
+        status_code = flask.Response(status=404)
+        return status_code
+    else:
+        return orders[order_id]
 
 @app.route('/pizza/update_order/<order_id>', methods = ['POST'])
 def update_order(order_id):
@@ -34,20 +38,15 @@ def update_order(order_id):
         status_code = flask.Response(status=404)
         return status_code
     content = request.get_json()
-    for order in orders:
-        for modified in content:
-            if modified == order:
-                orders[order] = content
-    print(orders)
+    orders[order_id] = content
     return "Update Done"
 
-@app.route('/pizza/cancle_order/<order_id>', methods = ['POST'])
+@app.route('/pizza/cancle_order/<order_id>', methods = ['DELETE'])
 def cancel_order(order_id):
     if order_id not in orders:
         status_code = flask.Response(status=404)
         return status_code
     del orders[order_id]
-    print(orders)
     return "Cancel Done"
 
 if __name__ == "__main__":
