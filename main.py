622da934d411a09acd636c19cee1c48706206743
Rui Zhu <zhuruisy@gmail.com>
2020-11-07 00:33:44 -0500

    main.py and new json format

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    notexit = True
+    while notexit:
+        print("welcom to Pizza shop What do you wanna do today ?")
+        delivery_method = input("1: creat pizza order, 2:modify order  3:submit order  4,exit\n")
+        if user_choice == "4":
+            break
+if __name__ == "__main__":
+    main()
